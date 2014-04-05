567bd53bf6d543fa61a73afea4521e87dd47d905
Theo Pavlakou <theo.pavlakou@gmail.com>
2014-04-05 17:18:35 +0100

    TPowerAlgorithm works for 1 PC - not for more than 1 yet though

--- TPowerAlgorithm.py
+++ TPowerAlgorithm.py
@@ -1,4 +1,5 @@
-from numpy import * 
+from numpy import dot 
+from numpy import array
 from scipy import * 
 from numpy.linalg import *
 from scipy.sparse import *
@@ -14,10 +15,81 @@ class TPowerAlgorithm:
     Outputs:
       u:      The v vector zeroed apart from the k highest elements.
     """
-    u = zeros(len(v))
-    sortedIndices = list(reversed(argsort(abs(v))))
-    vRestricted = v[sortedIndices[0:k]]
-    u[sortedIndices[0:k]] = double(vRestricted)/norm(vRestricted)
+    print("--- Truncate Operator ---")
+    u = zeros(v.shape[0])
+    print("--- Printing v --- ")
+    print(v)
+    # TODO make reversed
+    #sortedIndices = array(list(reversed(argsort(abs(v), axis=0))))
+    sortedIndices = argsort(abs(v), axis=0)
+    sortedIndices = fliplr(sortedIndices.T).T
+    print("Sorted: ")
+    print(sortedIndices)
+    vRestricted = v[sortedIndices[0:k],0]
+    normV = 0
+    if isinstance(vRestricted, csc_matrix):
+      normV = norm(vRestricted.todense())
+    else:
+      normV = norm(vRestricted)
+    if normV == 0:
+      return csc_matrix(zeros(v.shape[0]))
+    a = array(double(vRestricted))/normV
+    print("Printing a")
+    print(a)
+    print(a.shape)
+
+    print(sortedIndices)
+    u[sortedIndices[0:k]] = a[0:k, 0]
+    print(a[0:k, 0])
+    print(u)
     u = csc_matrix(u)
     return u
     
+  def getSparsePC(self, A, k):
+    
+    tolerance = 0.0000001
+    maxIterations = 50
+    # Initialisation
+    # TODO
+    # This initialises the first element to 1.
+    # Should be fixed.
+    x0 = zeros(A.shape[0])
+    x0[0] = 1
+
+    x = csc_matrix(x0)
+
+    # Power step
+    s = dot(A, transpose(x.todense()))
+    print("--- s vector is ----")
+    print(s)
+    g = 2*s
+    print("--- Calculating f ---")
+    f = dot(x.todense(), s)
+    print("--- Printing f ---")
+    print(f)
+
+    # Truncate step
+    x = self.truncateOperator(g, k)
+    fOld = f
+    i = 1
+
+    # Main Loop
+    while i <= maxIterations:
+      s = dot(A, transpose(x.todense()))
+      g = 2*s
+      x = self.truncateOperator(g, k)
+      print("--- Printing x ----")
+      print(x)
+      f = dot(x.todense(), s)
+
+      if (abs(f - fOld) < tolerance):
+        print("=== Breaking ===")
+        break
+
+      fOld = f
+      i += 1
+
+    return x, f
+
+
+      
